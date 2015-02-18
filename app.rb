e3f69115f04fd4a6c7583510e7ec92c8c6ba5cbc
karur4n <karuran.f@gmail.com>
2015-02-19 03:37:10 +0900

    [fix] ハッシュのタイプミスを修正

--- app.rb
+++ app.rb
@@ -3,9 +3,9 @@ Bundler.require
 require 'sinatra/reloader'
 
 client = Tumblr::Client.new({
-  :consumer_key => ENV['CONSUMER_KEY']
-  :consumer_secret => ENV['CONSUMER_SECRET']
-  :oauth_token => ENV['OAUTH_TOKEN']
+  :consumer_key => ENV['CONSUMER_KEY'],
+  :consumer_secret => ENV['CONSUMER_SECRET'],
+  :oauth_token => ENV['OAUTH_TOKEN'],
   :oauth_token_secret => ENV['OAUTH_TOKEN_SECRET']
 })
 
