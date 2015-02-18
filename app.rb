7c9c87d1c740c7f144196b86ce7f17b5aac39c9a
karur4n <karuran.f@gmail.com>
2015-02-19 03:27:39 +0900

    v0.0.1

---
+++ app.rb
@@ -0,0 +1,29 @@
+require 'bundler'
+Bundler.require
+require 'sinatra/reloader'
+
+client = Tumblr::Client.new({
+  :consumer_key => ENV['CONSUMER_KEY']
+  :consumer_secret => ENV['CONSUMER_SECRET']
+  :oauth_token => ENV['OAUTH_TOKEN']
+  :oauth_token_secret => ENV['OAUTH_TOKEN_SECRET']
+})
+
+
+get '/search/photo' do
+  results = client.tagged(params['q'])
+
+  urls = []
+
+  results.each do |result|
+    next unless result['type'] == 'photo'
+
+    photos = result['photos']
+
+    photos.each do |photo|
+      urls <<  photo['original_size']['url']
+    end
+  end
+
+  json urls
+end
